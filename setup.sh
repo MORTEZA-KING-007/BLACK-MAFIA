f7eba2d172bbc91a388c7277477c6038c16f9eaa
MORTEZA-ALIZADA <111222586+MORTEZA-KING-007@users.noreply.github.com>
2024-06-08 08:09:08 +0430

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,16 @@
+clear
+echo -e '\e[0m[✓] SETTING UP THE TOOL .....\n\n'
+sleep 1
+apt install jp2a -y
+apt install espeak -y
+apt install toilet -y
+apt install figlet -y
+gem install lolcat
+pip install pyfiglet
+pip install requests
+pip install colorama
+pip install bs4
+pip install futures
+clear
+sleep 2
+echo -e '\e[32mALL DONE ...'
